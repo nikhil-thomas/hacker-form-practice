aa9bd9ce53c68057008f486274131254101e4251
Nikhil Thomas <nikhilthomas1@gmail.com>
2020-11-02 00:45:44 +0530

    GOLANG | Add language switch
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

--- 03_golang/01_learn-go-with-tests/01_hello-world/hello/hello.go
+++ 03_golang/01_learn-go-with-tests/01_hello-world/hello/hello.go
@@ -1,13 +1,45 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	englishLanguage    = "english"
+	spanishLanguage    = "spanish"
+	frenchLanguage     = "french"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func main() {
 	name := "World"
-	fmt.Println(Hello(name))
+	fmt.Println(Hello(name, "english"))
 }
 
 // Hello prints greeting
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return fmt.Sprintf("%s%s", greeting(language), name)
+}
+
+func greeting(language string) string {
+	greeting := ""
+
+	switch language {
+	case spanishLanguage:
+		greeting = spanishHelloPrefix
+
+	case frenchLanguage:
+		greeting = frenchHelloPrefix
+
+	default:
+		greeting = englishHelloPrefix
+	}
+
+	return greeting
 }
--- 03_golang/01_learn-go-with-tests/01_hello-world/hello/hello_test.go
+++ 03_golang/01_learn-go-with-tests/01_hello-world/hello/hello_test.go
@@ -3,17 +3,47 @@ package hello_test
 import (
 	"fmt"
 	"testing"
+
 	"github.com/nikhil-thomas/hacker-form-practice/03_golang/01_learn-go-with-tests/01_hello-world/hello"
 )
 
 func TestHello(t *testing.T) {
-	name := "Chris"
-	want := fmt.Sprintf("Hello, %s", name)
-	got := hello.Hello(name)
+	t.Run("saying Hello to people", func(t *testing.T) {
+		name := "Chris"
+		language := "english"
+		want := fmt.Sprintf("Hello, %s", name)
+		got := hello.Hello(name, language)
 
+		assertCorrectMessage(t, want, got)
+	})
+	t.Run("saying Hello, World when empty string is supplied", func(t *testing.T) {
+		name := ""
+		language := "english"
+		want := fmt.Sprintf("Hello, World")
+		got := hello.Hello(name, language)
+
+		assertCorrectMessage(t, want, got)
+	})
+	t.Run("greeting in Spanish", func(t *testing.T) {
+		name := "Chris"
+		want := fmt.Sprintf("Hola, %s", name)
+		got := hello.Hello(name, "spanish")
+
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("greeting in French", func(t *testing.T) {
+		name := "Chris"
+		want := fmt.Sprintf("Bonjour, %s", name)
+		got := hello.Hello(name, "french")
+
+		assertCorrectMessage(t, want, got)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, want, got string) {
+	t.Helper()
 	if want != got {
 		t.Errorf("want: %s, got: %s", want, got)
 	}
 }
-
-
